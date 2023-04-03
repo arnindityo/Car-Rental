a2043fa27bc076f5e6b16a57b2464a6c47d4d695
arnindityo <arnindityon@gmail.com>
2023-04-03 23:38:56 +0700

    updates

--- src/components/cardmobil.js
+++ src/components/cardmobil.js
@@ -1,45 +1,80 @@
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useState, useEffect } from "react";
+import axios, { Axios } from "axios";
 
-const apiCarURL = "https://bootcamp-rent-cars.herokuapp.com/customer/v2/car";
-const [listCars, setListCars] = useState([]);
-const handlePilihMobil = (param) => {push(`/cari-mobil/${param}`)}
+const CardMobil = () => {
+  const apiCarURL = "https://bootcamp-rent-cars.herokuapp.com/customer/v2/car";
+  const [valueCategory, setCategory] = useState("");
+  const [valueHarga, setHarga] = useState("");
+  const [valueStatus, setStatus] = useState("");
+  const [carName, setCarName] = useState("");
+  const [listCars, setListCars] = useState([]);
+  console.log(listCars);
 
-function CardMobil() {
-  const handleOnClick = () => {
-    fetch(
-      apiCarURL +
-        "?" +
-        new URLSearchParams({
-          name: carName,
-          category: valueCategory,
-          isRented: valueStatus,
-          minPrice: valueHarga,
-          page: 1,
-          pageSize: 10,
-        })
-    )
-      .then((res) => res.json())
-      .then((resp) => {
-        const dataCar = resp.cars;
-        setListCars(dataCar);
-      });
-  };
-
-  {
-    listCars.map(({ id, image, name, price }) => {
-      return (
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src={image} />
-          <Card.Body>
-            <Card.Title>{name}</Card.Title>
-            <Card.Text>{price}</Card.Text>
-            <Button onClick={() => handlePilihMobil} variant="primary">Pilih Mobil</Button>
-          </Card.Body>
-        </Card>
-      );
-    });
+const handlerGetCar = async() => {
+try{
+  const config = {
+    headers: {
+    access_token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGJjci5pbyIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY4MDUxMjcxN30.Ad5wk2qarIO_Uc_DKiNmbi6c4q1QFfERpyU0ApIKBT8"
+    }
   }
+  const getCar = await axios.get(apiCarURL, config)
+  setListCars(getCar.data.cars)
+} catch(error){
+  console.log(error)
 }
+}
+
+  useEffect(() => {
+    // handleOnClick()
+    handlerGetCar()
+  },[])
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        {listCars.map(( item ) =>
+         ( 
+        <div class="card" key={item.id}>
+          <div style={{ width: "270px", height: "auto" }}>
+            <img
+              class="card-img-top"
+              width="100%"
+              height="auto"
+              src={item.image}
+              alt=""
+            />
+          </div>
+          <div class="card-body">
+            <h4 class="card-title">{item.name}</h4>
+            <p class="card-text">{item.price}</p>
+          </div>
+
+          <Button
+            // onClick={() => handlePilihMobil(id)}
+            style={{
+              margin: "1em",
+              backgroundColor: "#5CB85F",
+              border: "none",
+              borderRadius: "2px",
+            }}
+          >
+            Pilih Mobil
+          </Button>
+        </div>
+        )
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default CardMobil;
--- src/pages/pencarian.js
+++ src/pages/pencarian.js
@@ -1,20 +1,106 @@
-import { catOptions, hargaOptions, statusOptions } from "../constants";
+// import { catOptions, hargaOptions, statusOptions } from "../constants";
+// import React from "react";
+// import Navbar from "../components/navbar";
+// import Section7 from "../components/section7";
+// // import CardMobil from "../components/cardmobil";
+// import Textfield from "../components/form/Textfield";
+// import Select from "../components/form/Select";
+// import { useState } from "react";
+
+// const Pencarian = () => {
+//   const apiCarURL = "https://bootcamp-rent-cars.herokuapp.com/customer/v2/car";
+//   const [valueCategory, setCategory] = useState("");
+//   const [valueHarga, setHarga] = useState("");
+//   const [valueStatus, setStatus] = useState("");
+//   const [carName, setCarName] = useState("");
+//   const [listCars, setListCars] = useState([]);
+
+//   const handleOnClick = () => {
+//     fetch(
+//       apiCarURL +
+//         "?" +
+//         new URLSearchParams({
+//           name: carName,
+//           category: valueCategory,
+//           isRented: valueStatus,
+//           minPrice: valueHarga,
+//           page: 1,
+//           pageSize: 10,
+//         })
+//     )
+//       .then((res) => res.json())
+//       .then((resp) => {
+//         const dataCar = resp.cars;
+//         setListCars(dataCar);
+//       });
+//   };
+//   return (
+//     <div className="">
+//       <Navbar />
+//       <div className="container d-flex justify-content-center" style={{marginTop: "200px"}}>
+//         <div className="cari-mobil d-flex gap-2">
+//           <Textfield carName={carName} setCarName={setCarName} />
+//           <Select
+//             labelValue={"Kategori"}
+//             items={catOptions}
+//             onChange={setCategory}
+//             value={valueCategory}
+//           />
+//           <Select
+//             labelValue={"Harga"}
+//             items={hargaOptions}
+//             onChange={setHarga}
+//             value={valueHarga}
+//           />
+//           <Select
+//             labelValue={"Status"}
+//             items={statusOptions}
+//             onChange={setStatus}
+//             value={valueStatus}
+//           />
+//           <button
+//             onClick={() => {
+//               handleOnClick();
+//             }}
+//             type="button"
+//             className="btn btn-secondary mt-"
+//           >
+//             Edit
+//           </button>
+//         </div>
+//       </div>
+//       {/* <CardMobil /> */}
+//       <Section7 />
+//     </div>
+//   );
+// };
+
+// export default Pencarian;
+
 import React from "react";
-import Navbar from "../components/navbar";
-import Section7 from "../components/section7";
-// import CardMobil from "../components/cardmobil";
+import Navbar from "../components/navbar"
+import { catOptions, hargaOptions, statusOptions } from "../constants";
 import Textfield from "../components/form/Textfield";
 import Select from "../components/form/Select";
 import { useState } from "react";
-
-const apiCarURL = "https://bootcamp-rent-cars.herokuapp.com/customer/v2/car";
+import { useHistory } from "react-router-dom";
+import CardMobil from "../components/cardmobil";
+import Section7 from "../components/section7"
 
 const Pencarian = () => {
+  const apiCarURL = "https://bootcamp-rent-cars.herokuapp.com/customer/v2/car";
   const [valueCategory, setCategory] = useState("");
   const [valueHarga, setHarga] = useState("");
   const [valueStatus, setStatus] = useState("");
   const [carName, setCarName] = useState("");
   const [listCars, setListCars] = useState([]);
+  const { push } = useHistory();
+  console.log({ listCars });
+  
+  
+  const handlePilihMobil = (param) => {
+    push(`/cari-mobil/${param}`);
+  };
 
   const handleOnClick = () => {
     fetch(
@@ -26,7 +112,7 @@ const Pencarian = () => {
           isRented: valueStatus,
           minPrice: valueHarga,
           page: 1,
-          pageSize: 10,
+          pageSize: 50,
         })
     )
       .then((res) => res.json())
@@ -35,8 +121,9 @@ const Pencarian = () => {
         setListCars(dataCar);
       });
   };
+
   return (
-    <div className="">
+    <div>
       <Navbar />
       <div className="container d-flex justify-content-center" style={{marginTop: "200px"}}>
         <div className="cari-mobil d-flex gap-2">
@@ -62,16 +149,19 @@ const Pencarian = () => {
           <button
             onClick={() => {
               handleOnClick();
+              push("/pencarian");
             }}
             type="button"
-            className="btn btn-secondary mt-"
+            className="btn btn-success mt-"
           >
-            Edit
+            Cari Mobil
           </button>
+
         </div>
       </div>
-      {/* <CardMobil /> */}
-      <Section7 />
+            <CardMobil />
+            <br></br>
+            <Section7 />
     </div>
   );
 };
